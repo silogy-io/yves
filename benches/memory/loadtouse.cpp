df7fe9ffbfc04726695e6f498baec57ec6ca560a
James Connolly <james@silogy.io>
2024-06-21 17:21:44 -0400

    l2u

---
+++ benches/memory/loadtouse.cpp
@@ -0,0 +1,87 @@
+/*
+ * Single file microbenchmark for measuring load to use latency
+ *
+ *
+ * Instr stream will stride through an entire payload array ITERATIONS number of
+ * times
+ *
+ *
+ *
+ * parameters:
+ *
+ * RSS will determine size of the working set -- it is declared in kilobytes
+ *
+ * STRIDE will determine the "load distance" -- it is declared in bytes, and
+ * must be aligned to the size of a uint64_t
+ *
+ * ITERATIONS will determine how many times the working set is
+ *
+ */
+
+#include <cstdint>
+
+#ifndef RSS
+#define RSS 32
+#endif
+
+#ifndef STRIDE
+#define STRIDE 32
+#endif
+
+#ifndef ITERATIOS
+#define ITERATIONS 1000
+#endif
+
+#ifndef UNROLL_FACTOR
+#define UNROLL_FACTOR 8
+#endif
+
+// Stride is declared in bytes
+template <uint64_t size, uint64_t stride> struct StridedPtrChase {
+  uint64_t payload[size];
+  constexpr StridedPtrChase() {
+    static_assert(stride % sizeof(uint64_t) == 0,
+                  "Memory stride must be modulo 8");
+    static_assert(stride >= sizeof(uint64_t),
+                  "Memory stride must be greater than the size of a uint64_t");
+    auto stride_as_word = stride;
+    for (auto i = 0; i < size; i++) {
+      payload[i] = (i * sizeof(uint64_t) + stride_as_word) % size;
+    }
+  }
+};
+
+constexpr auto RSS_AS_KB = RSS * 1024 / sizeof(uint64_t);
+constexpr auto array = StridedPtrChase<RSS_AS_KB, STRIDE>();
+// number of loads that need to occur to stride across the entire working set
+// size
+constexpr auto LOADS_PER_ITERATION = RSS * 1024 / STRIDE;
+
+static_assert(LOADS_PER_ITERATION % UNROLL_FACTOR == 0,
+              "Unroll factor MUST must divide loads per iteration evenly");
+
+template <uint64_t depth>
+// explicit load unroller
+struct LoadFunroller {
+  static const volatile uint64_t *generate(const volatile uint64_t *baseline,
+                                           const volatile uint64_t *p) {
+    p = reinterpret_cast<const volatile uint64_t *>(
+        reinterpret_cast<uint64_t>(baseline) + *p);
+    if constexpr (depth < UNROLL_FACTOR) {
+      return LoadFunroller<depth + 1>::generate(baseline, p);
+    } else {
+      return p;
+    }
+  }
+};
+
+int main() {
+  const volatile uint64_t *p = &array.payload[0];
+  const volatile uint64_t *baseline = &array.payload[0];
+
+  for (int i = 0; i < ITERATIONS; i++) {
+    for (int j = 0; j < LOADS_PER_ITERATION; j++) {
+      p = LoadFunroller<1>::generate(baseline, p);
+    }
+  }
+}
---
+++ benches/memory/loadtouse_tl.py
@@ -0,0 +1,49 @@
+from pysmelt.importer import Target, init_local_rules
+from pysmelt.default_targets import raw_bash_build
+from pysmelt.path_utils import get_git_root
+
+mod = init_local_rules()
+
+from compile import download_zig, compile_local_ubench_zig
+from macos_profiler import mac_local_benchmark
+
+cpp_compiler = download_zig(name="cpp_compiler")
+compiler_path = cpp_compiler.get_outputs()["compiler"]
+
+bench_name = "loadtouse.cpp"
+sizes = [2**n for n in range(2, 4)]
+iterations = 100
+
+
+src_path = f"{get_git_root()}/profilers"
+
+mac_sources = " ".join([f"{src_path}/cJSON.c", f"{src_path}/mac_profiler.c"])
+
+
+profile_obj = raw_bash_build(
+    name="build_mac_profiler",
+    cmds=[
+        f"./{compiler_path} cc -march=native -O3 -fPIC -shared {mac_sources} -o profile.so"
+    ],
+    deps=[cpp_compiler.as_ref],
+    outputs={"profile_bin": "profile.so"},
+)
+
+
+profiler_bin = profile_obj.get_outputs()["profile_bin"]
+
+
+for size in sizes:
+    benchmark = compile_local_ubench_zig(
+        name=f"local_ptr_chase_{size}_kb",
+        compiler_path=compiler_path,
+        benchmark_path=bench_name,
+        ubench_parameters={},
+        compiler_target=cpp_compiler.as_ref,
+    )
+    bench_bin = benchmark.get_outputs()["binary"]
+    mac_local_benchmark(
+        name=f"local_ptr_chase_{size}_kb_mac",
+        profiler_path=profiler_bin,
+        benchmark_path=bench_bin,
+    )
